c1ad812e37b15ec66bad26ee4af1c06f420bf62b
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-09 20:06:44 +0530

    test case created for data insertion in Customer

--- VehicleServiceStation/src/main/java/com/project/services/getcustomerTest.java
+++ VehicleServiceStation/src/main/java/com/project/services/getcustomerTest.java
@@ -3,6 +3,7 @@ package com.project.services;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,7 @@ import com.project.entity.Customer;
 class getcustomerTest {
 
 	@Test
-	public void customer() {
+	public void getAllCustomers() {
 		List<Customer> customerList = new ArrayList<>();
 		try (CustomerDao customerDao = new CustomerDao()){
 			customerDao.getAllCustomer(customerList);
@@ -22,6 +23,30 @@ class getcustomerTest {
 		} catch (SQLException e) {
 			e.printStackTrace();
 			
+		}
+	}
+	
+	@Test
+	public void addCustomer() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Customer id here =");
+		int id=	sc.nextInt();
+		System.out.println("Enter Customer name here =");
+		String name=sc.next();
+		System.out.println("Enter Customer mobile here =");
+		String mobile=sc.next();
+		System.out.println("Enter Customer email here =");
+		String email=sc.next();
+		System.out.println("Enter Customer address here =");
+		String address=sc.next();
+		System.out.println("data inserted successfully....!!!");
+		
+		
+		Customer customer=new Customer(id,name,mobile,email,address);
+		try (CustomerDao customerDao = new CustomerDao()){
+			customerDao.addCustomer(customer);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 	
