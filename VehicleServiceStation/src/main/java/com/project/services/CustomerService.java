04f338f8ff502deb33554de855b82910ddbe3322
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-09 12:55:06 +0530

    saved changes

--- VehicleServiceStation/src/main/java/com/project/services/CustomerService.java
+++ VehicleServiceStation/src/main/java/com/project/services/CustomerService.java
@@ -9,6 +9,9 @@ import com.project.dao.CustomerDao;
 import com.project.entity.Customer;
 
 public class CustomerService {
+	
+	
+	//display all Customers
 	public static void getAllCustomer() {
 		List<Customer> customerList = new ArrayList<>();
 		try (CustomerDao customerDao = new CustomerDao()){
