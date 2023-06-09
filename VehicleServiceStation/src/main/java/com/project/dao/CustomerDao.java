4b91656579ce7b1103f7108beb16f613c8b6767b
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-09 11:40:30 +0530

    customerDao done & getter and setters created in Customer

--- VehicleServiceStation/src/main/java/com/project/dao/CustomerDao.java
+++ VehicleServiceStation/src/main/java/com/project/dao/CustomerDao.java
@@ -1,5 +1,32 @@
 package com.project.dao;
 
-public class CustomerDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import com.project.entity.Customer;
+import com.project.util.DBUtil;
+
+public class CustomerDao implements AutoCloseable{
+	
+	private Connection connection;
+	 public CustomerDao() throws SQLException {
+		 this.connection=DBUtil.getConnection();
+	 }
+	 public void getAllCustomer(List<Customer>customerList) throws SQLException {
+		 	String sql="SELECT * FROM customer";
+		 	try(PreparedStatement getAllCustomer=this.connection.prepareStatement(sql)){
+		 		ResultSet rs=getAllCustomer.executeQuery();
+		 		while (rs.next()) {
+					Customer customer=new Customer(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));
+					customerList.add(customer);
+					}
+		 	}
+	 }
+	@Override
+	public void close() throws Exception {
+			this.connection.close();
+}
 }
