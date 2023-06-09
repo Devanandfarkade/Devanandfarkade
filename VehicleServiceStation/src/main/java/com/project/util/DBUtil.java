324e54ca20672258204f91b4e84c4ddeb04c1649
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-09 11:07:21 +0530

    DBUTil created for database connection

--- VehicleServiceStation/src/main/java/com/project/util/DBUtil.java
+++ VehicleServiceStation/src/main/java/com/project/util/DBUtil.java
@@ -1,5 +1,23 @@
 package com.project.util;
 
-public class DBUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBUtil {
+	private static final String DRIVER="com.mysql.cj.jdbc.Driver";
+	private static String URL="jdbc:mysql://localhost:3306:/vehicle_db";
+	private static String USERNAME="root";
+	private static String PASSWORD="root";
+	
+	static {
+		try {
+			Class.forName(DRIVER);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() throws SQLException {
+			return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+	}
 }
