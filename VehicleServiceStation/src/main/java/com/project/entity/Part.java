4dc967a0656b5b5fc02db3809365d8b870c31def
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-12 23:55:52 +0530

    Part class implemented

--- VehicleServiceStation/src/main/java/com/project/entity/Part.java
+++ VehicleServiceStation/src/main/java/com/project/entity/Part.java
@@ -2,4 +2,52 @@ package com.project.entity;
 
 public class Part {
 
+	private int id;
+	private String name;
+	private String description ;
+	private String price;
+	
+	public Part() {
+	
+	}
+	
+	public Part(int id, String name, String description, String price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Part [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+	
+	
 }
