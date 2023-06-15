e9ee7ae62c7e6a6b5a82e041b66782320f0c9716
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-15 15:51:18 +0530

    get all vehicles test case created

--- VehicleServiceStation/src/test/java/testCases/VehicleDaoTest.java
+++ VehicleServiceStation/src/test/java/testCases/VehicleDaoTest.java
@@ -1,6 +1,8 @@
 package testCases;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +20,21 @@ public class VehicleDaoTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	@Test
+	public void testGetAllVehicles() {
+		VehicleDao vehicleDao;
+		try {
+			vehicleDao =new VehicleDao();
+			List<Vehicle>vehicleList=new ArrayList<>();
+			vehicleDao.getAllVehicle(vehicleList);
+			vehicleList.forEach(System.out::println);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
