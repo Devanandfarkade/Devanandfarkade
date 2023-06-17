71c83fc472f04c47eef4dc0ba7560ffb735c996c
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-17 11:45:50 +0530

    comments deleted in selectCusomer

--- VehicleServiceStation/src/main/java/com/project/services/ServiceReqService.java
+++ VehicleServiceStation/src/main/java/com/project/services/ServiceReqService.java
@@ -25,37 +25,9 @@ public class ServiceReqService {
 		System.out.println("Please Enter 0 to go back ans 1st you have to enter Customer_vehiles ");
 		return null;
 	}
-//		if (vehicleNo == null) {
-//		
-//			System.out.print("Enter Customer ID = ");
-//			String customer_id = new Scanner(System.in).next();
-//			System.out.println("Enter Vehicle ID =");
-//			String vhicle_id = new Scanner(System.in).next();
-//			
-//			VehicleService.getAllVehicle();
-//			System.out.println("Select The Vehicle --> ");
-//			System.out.println("Enter The Vehicle id or Enter 0 for adding the company & Model ");
-//			int id=new Scanner(System.in).nextInt();
-//			
-//			if(id == 0) {
-//				System.out.println("Enter Company name =");
-//				String company=new Scanner(System.in).next();
-//				System.out.println("Enter Company name =");
-//				String model=new Scanner(System.in).next();
-//			}
-//			VehicleDao vehicleDao = new VehicleDao();
-//
-//			vehicleDao.addVehicle(new Vehicle(company, model));
-//			System.out.println("Vehicle Added ....!!!");
-//
-//		} else {
-//			VehicleDao vehicleDao = new VehicleDao();
-//			vehicleDao.customerVehicle(new CustomerVehicle(vehicleNo, vehicleNo,id));
-//
-//			System.out.println("Vehicle  Added.....!!!! ");
-//
-//		}
+
 	}
+	
 
 	public static void processRequest() {
 		if (ServiceReqService.selectCusomer() == null) {
