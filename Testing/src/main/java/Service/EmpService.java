b61a6a679ac2e0f7fdd78570edb42b1c887c67a6
Devanandfarkade <115337013+Devanandfarkade@users.noreply.github.com>
2023-06-30 15:48:29 +0530

    testing commit

--- Testing/src/main/java/Service/EmpService.java
+++ Testing/src/main/java/Service/EmpService.java
@@ -1,10 +1,24 @@
 package Service;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 
 
 public class EmpService {
+	public static void addEmployee() {
+		Scanner choice = new Scanner(System.in);
+		System.out.println("Enter Employee id  =");
+		int empno = choice.nextInt();
+		System.out.println("Enter Employee name =");
+		String name = choice.next();
+		System.out.println("Enter Employee job =");
+		String mgr = choice.next();
+		System.out.println("Enter Employee mgr =");
+		Date hiredate  = choice.();
+		String address = choice.next();
+		
+	}
 	
 
 	
